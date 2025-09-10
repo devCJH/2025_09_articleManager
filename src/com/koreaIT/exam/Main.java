c44abfcd1ff562ec1857914a3dd8d062b8106e0b
devCJH <http-@naver.com>
2025-09-10 10:47:17 +0900

    게시판 틀잡기

---
+++ src/com/koreaIT/exam/Main.java
@@ -0,0 +1,24 @@
+package com.koreaIT.exam;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+			
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
+		
+		System.out.println("== 프로그램 종료 ==");
+		
+		sc.close();
+	}
+}

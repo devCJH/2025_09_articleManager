a1ca124bb64ff60d1ea9b159c4f324e2b0371fad
devCJH <http-@naver.com>
2025-09-11 10:44:47 +0900

    article detail

--- src/com/koreaIT/exam/Main.java
+++ src/com/koreaIT/exam/Main.java
@@ -21,20 +21,12 @@ public class Main {
 				break;
 			}
 			
-			if (cmd.equals("article list")) {
-				
-				if (articles.size() == 0) {
-					System.out.println("게시글이 존재하지 않습니다");
-					continue;
-				}
-				
-				System.out.println("번호	|	제목");
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
-					System.out.printf("%d	|	%s\n", article.id, article.title);
-				}
-				
-			} else if (cmd.equals("article write")) {
+			if (cmd.trim().length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			if (cmd.equals("article write")) {
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -46,11 +38,45 @@ public class Main {
 				articles.add(article);
 				
 				System.out.println(lastArticleId + "번 게시글이 작성되었습니다");
+			} else if (cmd.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 존재하지 않습니다");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles) {
+					if (id == article.id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if (foundArticle == null) {
+					System.out.println("게시글 없음");
+					continue;
+				}
+				
+				System.out.printf("== %d번 게시글 상세보기 ==\n", id);
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
-		
 		System.out.println("== 프로그램 종료 ==");
-		
 		sc.close();
 	}
 }
